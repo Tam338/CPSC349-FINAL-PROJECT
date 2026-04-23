84e2c23f4e5f9c5038e8dff532f62cdb850422ba
dylan <tvnguyen@csu.fullerton.edu>
2026-04-22 19:52:42 -0700

    finish styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
 import background from './assets/tux-linux.png'
+import { Timer } from 'lucide-react'
+import { Calendar } from 'lucide-react'
+import { Music } from 'lucide-react'
+import { ClipboardList } from 'lucide-react'
+
 import './App.css'
 
 function App() {
-
+  
   return (
     <>
-      <section id="header">
-        <div>
-          <button>Timer</button>
-          <button>Calendar</button>
-          <button>do-do list</button>
-          <button>memo</button>
-          <button>music</button>
-        </div>
-      </section>
+      <div className="main-page">
+        <nav className="header">
+          <button><Timer size={50}/></button>
+          <button><Calendar size={50}/></button>
+          <button><Music size={50}/></button>
+          <button><ClipboardList size={50}/></button>
+        </nav>
+      </div>
     </>
   )
 }
