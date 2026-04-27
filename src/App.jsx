8bd25d30c112cbbef4d83c587aba64792cfdff38
dylan <tvnguyen@csu.fullerton.edu>
2026-04-26 23:34:21 -0700

    completed with timer button

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,111 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Calendar from 'react-calendar'
 
-import TimerButton from './TimerButton.jsx'
-import CalendarButton from './CalendarButton.jsx'
-import MusicButton from './MusicButton.jsx'
-import ListButton from './List.jsx'
+import {Timer, Pause, Play, RotateCcw, Music, ClipboardList, Calendar as CalendarIcon } from 'lucide-react';
 
 import './App.css'
 
 function App() {
 
   const [timerButtons, setTimerButtons] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const [iniTime, setIniTime] = useState(0);
 
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  useEffect( () => {
+    let id;
+    if(isRunning && time > 0)
+    {
+      id = setInterval( () => {     
+        setTime( (prev) => prev - 1000)
+      }, 1000)
+    } else if(time === 0)
+    {
+      setIsRunning(false);
+    }
+
+    return () => clearInterval(id);
+
+  }, [isRunning, time] )
+
+  function formatTime(){
+    let minutes = Math.floor(time / (1000 * 60) % 60);
+    let seconds = Math.floor(time / (1000) % 60);
+
+    minutes = String(minutes).padStart(2, '0');
+    seconds = String(seconds).padStart(2, '0'); 
+
+    return `${minutes}:${seconds}`;  
+  }
 
   return (
     <>
       <div className="main-page">
         <nav className="header">
-          <TimerButton 
-            onMouseEnter={() => setTimerButtons(true)}
-            onMouseLeave={ () => setTimerButtons(false) }/>
-          <CalendarButton/>
-          <MusicButton/>
-          <ListButton/>
+          <button 
+            onClick={ () => setTimerButtons(!timerButtons) }>
+          <Timer size={50}/></button>
+
+          <button onClick={ () => setShowCalendar(!showCalendar) }><CalendarIcon size={50}/></button>
+          <button><Music size={50}/></button>
+          <button><ClipboardList size={50}/></button>
         </nav>
 
         { timerButtons && (
-          <div className='timer-buttons'>
+          <div className='timer-buttons'
+            >
             <div>
-              <button>15</button>
-              <button>30</button>
-              <button>45</button>
-              <button>60</button>
+              <button
+                onClick={ () => {
+                  setTime(900000);
+                  setIniTime(900000);
+                  setIsRunning(true)
+                  setTimerButtons(!timerButtons)
+                }}>15</button>
+              <button
+                onClick={ () => {
+                  setTime(1800000);
+                  setIniTime(1800000);
+                  setIsRunning(true)
+                  setTimerButtons(!timerButtons)
+                }}>30</button>
+              <button
+                onClick={ () => {
+                  setTime(2700000);
+                  setIniTime(2700000);
+                  setIsRunning(true)
+                  setTimerButtons(!timerButtons)
+                }}>45</button>
+              <button
+                onClick={ () => {
+                  setTime(3599000);
+                  setIniTime(3599000);
+                  setIsRunning(true)
+                  setTimerButtons(!timerButtons)
+                }}>60</button>
             </div>
             <div>
-              <button>pause</button>
-              <button>restart</button>
+              <button onClick={ () => setIsRunning(true)}><Play/></button>
+              <button onClick={ () => setIsRunning(false)}><Pause/></button>
+              <button onClick={ () => setTime(iniTime)}><RotateCcw/></button>
             </div>
           </div>
         )}
 
-        <div className='display'>
-          <h1>something</h1>
+        <div  className='display'>
+          { (time > 0) &&
+            <div>
+              <h1>{formatTime()}</h1>
+            </div>
+          }
+          
+          { showCalendar && 
+            <div>
+              <Calendar/>
+            </div>
+          }
         </div>
       </div>
     </>
